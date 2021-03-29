cfe9209b02d2d1776e548b59824cde8fe01b3cf0
Elham13 <elhamuddin.mahmoodi@gmail.com>
2021-03-29 12:13:58 +0530

    d

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -281,7 +281,8 @@ const downloadPageAsPdf = (pageSection) => {
 
 const downloadPageAsExcel = (tableID, filename = '') => {
   var downloadurl;
-    var dataFileType = 'application/vnd.ms-excel';
+    // var dataFileType = 'application/vnd.ms-excel';
+    var dataFileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
     var tableSelect = document.getElementById(tableID);
     var tableHTMLData = tableSelect.outerHTML.replace(/ /g, '%20');
     
@@ -306,6 +307,40 @@ const downloadPageAsExcel = (tableID, filename = '') => {
         //triggering the function
         downloadurl.click();
     }
+}
+
+const fnExcelReport = () => {
+    var tab_text="<table border='2px'><tr bgcolor='#87AFC6'>";
+    var textRange; 
+    var j=0;
+    tab = document.getElementById('wpTable'); // id of table
+
+    for(j = 0 ; j < tab.rows.length ; j++) 
+    {     
+        tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
+        //tab_text=tab_text+"</tr>";
+    }
+
+    tab_text=tab_text+"</table>";
+    tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+    tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE "); 
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html","replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus(); 
+        sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
+    }  
+    else                 //other browser not tested on IE 11
+        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));  
+
+    return (sa);
 }
 
 const hideAndShowCustomModal = (opentBtns, modalToOpen) => {
